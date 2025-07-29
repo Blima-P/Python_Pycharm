707107174a7204cd6fde76800be492b08cc5c7a7
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-07-29 14:01:19 -0300

    Add files via upload

---
+++ Entrada_de_dados.py
@@ -0,0 +1,19 @@
+salario1 : float; salario2 : float
+nome1 : str; nome2 : str; sexo : str
+idade : int
+
+nome1 = input("Nome da primeira pessoa: ")
+salario1 = float(input("salario da primeira pessoa"))
+
+nome2 = input("Nome da segunda pessoa: ")
+salario2 = float(input("salario da segunda pessoa"))
+
+idade = int(input("digite uma idade: "))
+sexo = input ("Digite um sexo (F/m): ")
+print(f"nome1 : {nome1}")
+print(f"salario1: {salario1}")
+print(f"nome2 : {nome2}")
+print(f"salario2: {salario2}")
+
+print (f"idade : {idade}")
+print (f"Sexo : {sexo}")
