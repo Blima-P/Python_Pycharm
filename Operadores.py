8a8aa7f8ff136f54e379c469df9490d5d38b3e97
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-07-29 13:13:35 -0300

    Add files via upload

---
+++ Operadores.py
@@ -0,0 +1,29 @@
+x = 5
+y = 2
+
+## Operadores Aritméticos
+z = 5 % 2
+z = 5 ** 2
+z = 5 / 2
+z = 5 // 2
+z = 5 * 2
+z = 5 - 2
+z = 5 + 2
+
+
+## Operadores de Comparação
+z = 5 == 2
+z = 5 != 2
+z = 5 > 2
+z = 5 < 2
+z = 5 >= 2
+z = 5 <= 2
+z = 5 is 2
+z = 5 is not 2
+## Operadores Lógicos
+z = 5 > 2 and 5 < 10
+z = 5 > 2 or 5 < 10
+z = not (5 > 2)
+
+
+print(z) 
