9fc7898b8b9de276ec55015051902ce68fac2d3d
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-07-29 13:07:01 -0300

    Add files via upload

---
+++ Variaveis_tipos.py
@@ -0,0 +1,17 @@
+idade : int
+salario : float
+altura : float
+genero : str
+nome : str
+
+idade = 20
+salario = 5800.5
+altura = 1.63
+genero = "F"
+nome = "Maria Silva"
+
+print (F"idade = {idade}")
+print (F"salario = {salario:.2f}")
+print (F"altura = {altura:.2f}")
+print (F"genero = {genero}")
+print (F"nome = {nome}")
