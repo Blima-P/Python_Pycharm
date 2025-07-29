9b0822e7e6b2bbd8b7c7bd73a0c51f2f91100586
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-07-29 13:24:20 -0300

    Add files via upload

---
+++ Saida-dados.py
@@ -0,0 +1,21 @@
+##x = "maria"
+##y = 19
+
+##print ("%s tem %d anos" % (x,y))
+
+##x: int; y:int
+##x = 10
+##y = 20
+
+##print(x)
+##print(y)
+
+##x: float
+##x = 2.3456
+##print("{:.2f}".format(x))
+
+x = "maria silva"
+y = "F"
+z = 4560.9
+i = 32
+print(f" A funcionaria {x}, sexo {y}, ganha {z:.2f} e tem {i} Anos")
